8700cef4609501ae3fc9e4d75f8e13d817cf61c2
Fadhil <79124345+fdhlnrzzmn@users.noreply.github.com>
2023-08-30 12:02:07 +0800

    style: move image to public folder

--- src/components/section/Projects.jsx
+++ src/components/section/Projects.jsx
@@ -10,40 +10,40 @@ const Projects = () => {
                 <p>Di halaman ini kamu akan menemukan proyek-<br />proyek yang pernah saya buat.</p>
             </div>
             <div className="project-category">
-                <p><img src="./src/assets/dot.svg" alt="" />Semua Kategori</p>
-                <p><img src="./src/assets/dot.svg" alt="" />UI Design</p>
-                <p><img src="./src/assets/dot.svg" alt="" />Web Development</p>
-                <p><img src="./src/assets/dot.svg" alt="" />Mobile Development</p>
+                <p><img src="./public/assets/dot.svg" alt="" />Semua Kategori</p>
+                <p><img src="./public/assets/dot.svg" alt="" />UI Design</p>
+                <p><img src="./public/assets/dot.svg" alt="" />Web Development</p>
+                <p><img src="./public/assets/dot.svg" alt="" />Mobile Development</p>
             </div>
         </div>
         <div className="projects-list">
             <div className="card">
-                <img src="./src/assets/project1.png" alt="" />
+                <img src="./public/assets/project1.png" alt="" />
                 <h4>Login Page UI Design</h4>
                 <p>UI DESIGN</p>
             </div>
             <div className="card">
-                <img src="./src/assets/project2.png" alt="" />
+                <img src="./public/assets/project2.png" alt="" />
                 <h4>Landing Page</h4>
                 <p>WEB DEVELOPMENT</p>
             </div>
             <div className="card">
-                <img src="./src/assets/project3.png" alt="" />
+                <img src="./public/assets/project3.png" alt="" />
                 <h4>Landing Page</h4>
                 <p>WEB DEVELOPMENT</p>
             </div>
             <div className="card">
-                <img src="./src/assets/project4.png" alt="" />
+                <img src="./public/assets/project4.png" alt="" />
                 <h4>Landing Page UI Design</h4>
                 <p>UI DESIGN</p>
             </div>
             <div className="card">
-                <img src="./src/assets/project5.png" alt="" />
+                <img src="./public/assets/project5.png" alt="" />
                 <h4>Mobile App</h4>
                 <p>MOBILE DEVELOPMENT</p>
             </div>
             <div className="card">
-                <img src="./src/assets/project6.png" alt="" />
+                <img src="./public/assets/project6.png" alt="" />
                 <h4>Login Page UI Design</h4>
                 <p>MOBILE DEVELOPMENT</p>
             </div>
--- src/components/section/Skills.jsx
+++ src/components/section/Skills.jsx
@@ -10,32 +10,32 @@ const Skills = () => {
                 </div>
                 <div className="skills-list">
                 <div className="skills-card">
-                    <img src="./src/assets/skills1.png" alt="" />
+                    <img src="./public/assets/skills1.png" alt="" />
                     <h4>Biasa Pake Terminal</h4>
                     <p>Udah terbiasa sih pake terminal kek gini,<br />karena lebih cepet kerjanya ketimbang<br />pake GUI-GUI begitu.</p>
                 </div>
                 <div className="skills-card">
-                    <img src="./src/assets/skills2.png" alt="" />
+                    <img src="./public/assets/skills2.png" alt="" />
                     <h4>Web Development</h4>
                     <p>Bikin web itu gampang, tinggal ketik-ketik kode doang dah jadi lah web. Apalagi sekarang bisa pake Wix hehe.</p>
                 </div>
                 <div className="skills-card">
-                    <img src="./src/assets/skills3.png" alt="" />
+                    <img src="./public/assets/skills3.png" alt="" />
                     <h4>Nulis Artikel</h4>
                     <p>Biasa nulis artikel di web sendiri, ya walaupun masih berantakan yang penting nulis sih, daripada nggak sama sekali.</p>
                 </div>
                 <div className="skills-card">
-                    <img src="./src/assets/skills2.png" alt="" />
+                    <img src="./public/assets/skills2.png" alt="" />
                     <h4>Web Development</h4>
                     <p>Bikin web itu gampang, tinggal ketik-ketik kode doang dah jadi lah web. Apalagi sekarang bisa pake Wix hehe.</p>
                 </div>
                 <div className="skills-card">
-                    <img src="./src/assets/skills3.png" alt="" />
+                    <img src="./public/assets/skills3.png" alt="" />
                     <h4>Nulis Artikel</h4>
                     <p>Biasa nulis artikel di web sendiri, ya walaupun masih berantakan yang penting nulis sih, daripada nggak sama sekali.</p>
                 </div>
                 <div className="skills-card">
-                    <img src="./src/assets/skills1.png" alt="" />
+                    <img src="./public/assets/skills1.png" alt="" />
                     <h4>Biasa Pake Terminal</h4>
                     <p>Udah terbiasa sih pake terminal kek gini,<br />karena lebih cepet kerjanya ketimbang<br />pake GUI-GUI begitu.</p>
                 </div>
